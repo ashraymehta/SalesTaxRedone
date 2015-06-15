b6e9bf3daaa954f8e35f27780026144e20589949
Ashray Mehta <ashraymehta@users.noreply.github.com>
2015-06-15 11:58:15 +0530

    App now calls parser to parse items

--- src/com/thoughtworks/pathashala/SalesTaxApp.java
+++ src/com/thoughtworks/pathashala/SalesTaxApp.java
@@ -1,14 +1,30 @@
 package com.thoughtworks.pathashala;
 
+import com.thoughtworks.pathashala.items.Item;
+
+import java.util.ArrayList;
+
 public class SalesTaxApp {
     private Console console;
+    private Parser parser;
 
-    public SalesTaxApp(Console console) {
+    public SalesTaxApp(Console console, Parser parser) {
         this.console = console;
+        this.parser = parser;
     }
 
 
     public void start() {
-        console.getUserInput();
+        String input = console.getUserInput();
+        String[] lines = getLines(input);
+        ArrayList<Item> items = new ArrayList<>();
+        for (int i = 0; i < lines.length; i++) {
+            Item item = parser.parse(lines[i]);
+            items.add(item);
+        }
+    }
+
+    private String[] getLines(String input) {
+        return input.split(System.lineSeparator());
     }
 }
--- test/com/thoughtworks/pathashala/SalesTaxAppTest.java
+++ test/com/thoughtworks/pathashala/SalesTaxAppTest.java
@@ -1,29 +1,51 @@
 package com.thoughtworks.pathashala;
 
+import com.thoughtworks.pathashala.items.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.*;
+import java.util.HashMap;
+
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SalesTaxAppTest {
-    @Mock
     Console console;
+    @Mock
+    Parser parser;
 
     private SalesTaxApp salesTaxApp;
 
     @Before
     public void setUp() throws Exception {
-        salesTaxApp = new SalesTaxApp(console);
-
+        HashMap<String, Item> stringItemHashMap = new HashMap<>();
+        stringItemHashMap.put("book", new BookItem());
+        stringItemHashMap.put("music", new OtherItem());
+        stringItemHashMap.put("chocolate", new FoodItem());
+        stringItemHashMap.put("perfume", new OtherItem());
+        stringItemHashMap.put("pills", new MedicalItem());
+        salesTaxApp = new SalesTaxApp(console, parser);
     }
 
     @Test
     public void shouldGetUserInput() throws Exception {
         salesTaxApp.start();
+        when(console.getUserInput()).thenReturn("1 book at 10.00");
         verify(console).getUserInput();
+    }
+
+    @Test
+    public void shouldGetItemFromParser() throws Exception {
+        PrintStream printStream = new PrintStream(System.out);
+        InputStreamReader in = new InputStreamReader(new ByteArrayInputStream("1 book at 10.00".getBytes()));
+        console = new Console(new BufferedReader(in), printStream);
+        salesTaxApp = new SalesTaxApp(console, parser);
+        salesTaxApp.start();
+        verify(parser).parse("1 book at 10.00");
     }
 }
