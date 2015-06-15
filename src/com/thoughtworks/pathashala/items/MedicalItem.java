99a3c5e61b4f6d3e79b7a3ebe59ee907e0874e58
Ashray Mehta <ashraymehta@users.noreply.github.com>
2015-06-15 10:51:30 +0530

    Added MeicalItem which can calculate its own sales tax

---
+++ src/com/thoughtworks/pathashala/items/MedicalItem.java
@@ -0,0 +1,26 @@
+package com.thoughtworks.pathashala.items;
+
+public class MedicalItem implements Item {
+    private boolean isImported;
+    private double price;
+
+    public MedicalItem(boolean isImported, double price) {
+        this.isImported = isImported;
+        this.price = price;
+    }
+
+    @Override
+    public boolean isImported() {
+        return false;
+    }
+
+    @Override
+    public double calculateBasicSalesTax() {
+        return 0;
+    }
+
+    @Override
+    public double calculateImportDuty() {
+        return 0;
+    }
+}
---
+++ test/com/thoughtworks/pathashala/items/MedicalItemTest.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.pathashala.items;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class MedicalItemTest {
+    @Test
+    public void shouldCalculateCorrectSalesTax() throws Exception {
+        MedicalItem medicalItem = new MedicalItem(false, 12.49);
+        double actualTax = medicalItem.calculateBasicSalesTax();
+        double expectedTax = 0.0d;
+
+        assertEquals(expectedTax, actualTax);
+    }
+}
